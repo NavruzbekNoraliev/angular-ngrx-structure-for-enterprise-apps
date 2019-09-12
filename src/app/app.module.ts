eb54621501d789bfb70e040d5b415a5c4452313e
Navruzbek <pacesetternavruzbek97@gmail.com>
2019-09-12 16:06:02 +0500

    upgrade to anfular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,11 @@ import { HeroSearchComponent } from './components/hero-search/hero-search.compon
       dataEncapsulation: false,
       delay: 300,
       passThruUnknownUrl: true
-    })
+    }),
+    // StoreDevtoolsModule.instrument({
+    //   maxAge: 25, // Retains last 25 states
+    //   logOnly: environment.production, // Restrict extension to log-only mode
+    // }),
   ],
   declarations: [
     AppComponent,
