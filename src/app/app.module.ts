df317da11d01ead3fc69afd71bd874d420e41361
Navruzbek <pacesetternavruzbek97@gmail.com>
2019-09-12 17:24:46 +0500

    Initial version for ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,6 +14,14 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroSearchComponent } from './components/hero-search/hero-search.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+//NGRX DevTools
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environment
+
+// NGRX
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   imports: [
@@ -26,10 +34,10 @@ import { HeroSearchComponent } from './components/hero-search/hero-search.compon
       delay: 300,
       passThruUnknownUrl: true
     }),
-    // StoreDevtoolsModule.instrument({
-    //   maxAge: 25, // Retains last 25 states
-    //   logOnly: environment.production, // Restrict extension to log-only mode
-    // }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   declarations: [
     AppComponent,
